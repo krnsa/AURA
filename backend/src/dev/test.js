c126f09c4820579dd7bcddf4d16e330cfb4e0566
sammulvey747 <99451072+sammulvey747@users.noreply.github.com>
2025-03-23 16:33:42 -0400

    Pushing test code

--- backend/src/dev/test.js
+++ backend/src/dev/test.js
@@ -24,19 +24,9 @@ import newPost from "../implementations/newPost.js";
 import getPosts from "../api/getPosts.js";
 import fs from "fs";
 import path from "path";
+import getPostByID from "../api/getPostByID.js";
+import removePost from "../implementations/removePost.js";
 
-newPost(1, "test");
 
-const filePath = path.resolve("./img/test.jpg");
-const fileBuffer = fs.readFileSync(filePath);
-
-// Create a Blob (Supabase accepts Blobs like Files)
-const file = new File([fileBuffer], "test-image.jpg", { type: "image/jpeg" });
-
-await newPost(3, "test 3");
-
-console.log(findUser("sam.mulvey747"));
-
-let data = await getPosts(3);
-
-console.log(data.length);
+//                      user_id, post_id
+const resp = await removePost(1, 21)
