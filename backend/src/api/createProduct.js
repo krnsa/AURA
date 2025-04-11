7a73d2ae271bcccdfe8a50c61f4c915ef96d5152
viraj0213 <virpatel2250@gmail.com>
2025-04-11 16:02:29 -0400

    Added three new files for product listing in backend

---
+++ backend/src/api/createProduct.js
@@ -0,0 +1,33 @@
+// import supabase client
+import supabase from "../supabase/supabaseClient.js";
+
+export default async function createProduct({ name, description, price, imageUrl }) {
+  if (!name || !price) {
+    console.log("Missing required fields: name or price.");
+    return;
+  }
+
+  try {
+    const { data, error } = await supabase.from("products").insert([
+      {
+        name,
+        description,
+        price,
+        image_url: imageUrl,
+      },
+    ]);
+
+    if (error) {
+      throw error;
+    } else if (!data || data.length === 0) {
+      console.log("Supabase query succeeded with empty response.");
+      return { message: "Insert successful but no data returned" };
+    } else {
+      console.log(data, "Supabase insert succeeded:");
+      return data[0];
+    }
+  } catch (err) {
+    console.error("Supabase insert failed:", err.message);
+    throw err;
+  }
+}
